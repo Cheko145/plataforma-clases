fab1aa6d4a2a2b9b55d4b4149f26e1d7610cd41b
Juan Pacheco <jl.quinonez20@info.uas.edu.mx>
2026-02-13 12:39:12 -0700

    Changes to deployment on Vercel

--- lib/db.ts
+++ lib/db.ts
@@ -7,6 +7,10 @@ export const pool =
   globalForPg.pool ??
   new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : undefined,
   });
 
 if (process.env.NODE_ENV !== "production") {
